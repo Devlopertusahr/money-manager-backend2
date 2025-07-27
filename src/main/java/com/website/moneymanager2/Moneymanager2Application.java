e6d0f1088bebf97e85f2848d2a5d7a744e335baa
TusharDevloper9 <tusshartiwari9229@gmail.com>
2025-07-28 01:19:39 +0530

    Fresh start

---
+++ src/main/java/com/website/moneymanager2/Moneymanager2Application.java
@@ -0,0 +1,13 @@
+package com.website.moneymanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moneymanager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moneymanager2Application.class, args);
+	}
+
+}
