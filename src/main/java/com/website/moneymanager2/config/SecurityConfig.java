56d89560edc17ffe87aa5db42e486748602eef50
DevloperTushar <219886536+Devlopertusahr@users.noreply.github.com>
2026-01-16 22:19:31 +0530

    Update SecurityConfig.java

--- src/main/java/com/website/moneymanager2/config/SecurityConfig.java
+++ src/main/java/com/website/moneymanager2/config/SecurityConfig.java
@@ -1,3 +1,27 @@
+package com.website.moneymanager2.config;
+
+import com.website.moneymanager2.security.JwtRequestFilter;
+import com.website.moneymanager2.service.AppUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -51,6 +75,3 @@ public class SecurityConfig {
         return new ProviderManager(authenticationProvider);
     }
 }
-
-
-
